57ea4b6bf8c3b55b9070d267900c368458fbf56e
Carson Smith <csm2356@wgu.edu>
2025-02-26 21:35:08 -0600

    added onloaded to body element. outlined plan for JS to make game function

---
+++ script.js
@@ -0,0 +1,22 @@
+/*
+ automatically start game when page is loaded
+ function to add an X for the player when they click a cell
+ function to check for a winner
+ function to start a new game
+ function to have computer select a cell and add an O in it
+ */
+function playerTurn() {
+
+}
+
+function computerTurn() {
+
+}
+
+function checkForWinner() {
+
+}
+
+function newGame() {
+
+}
